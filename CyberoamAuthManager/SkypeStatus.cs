79539274f4cd847a411191fec41782f7ade67ce3
shashi.k <sashikumar148@gmail.com>
2017-03-31 17:29:37 +0530

    update

---
+++ CyberoamAuthManager/SkypeStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SKYPE4COMLib;
+
+namespace CyberoamAuthManager
+{
+    static class SkypeStatus
+    {
+        private static bool ChageStatus(TUserStatus status)
+        {
+            try
+            {
+                var skype = new Skype();
+                skype.Attach(7, true);
+                skype.ChangeUserStatus(status);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static bool GoOnline()
+        {
+            return ChageStatus(TUserStatus.cusOnline);
+        }
+        public static bool GoAway()
+        {
+            return ChageStatus(TUserStatus.cusAway);
+        }
+    }
+}
--- SetupApplication/MainWindow.xaml.cs
+++ SetupApplication/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using SetupApplication.Code;
 
 namespace SetupApplication
@@ -56,7 +45,7 @@ namespace SetupApplication
             }
             finally
             {
-                this.Close();
+                Close();
             }
         }
 
@@ -73,7 +62,7 @@ namespace SetupApplication
             }
             finally
             {
-                this.Close();
+                Close();
             }
         }
 
@@ -95,7 +84,7 @@ namespace SetupApplication
             }
             finally
             {
-                this.Close();
+                Close();
             }
         }
     }
